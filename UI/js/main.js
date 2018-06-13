55f5ea41c45f58da19a8b4e7947b8147b7484034
John Obi <jovinxcreative@gmail.com>
2018-06-13 03:31:15 +0100

    register page is complete and form validates

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $('main.content .content-inner form button').on('click', function(event) {
+    $('main.content .content-inner form button#login-btn').on('click', function(event) {
         event.preventDefault();
         var err = '';
         var errorOutput = $('.content .content-inner form p.error-message');
@@ -32,5 +32,44 @@ $(document).ready(function () {
         }
 
         
+    });
+
+
+    $('main.content .content-inner form button#register-btn').on('click', function(event) {
+        event.preventDefault();
+
+
+        var err = '';
+        var errorOutput = $('.content .content-inner form p.error-message');
+        errorOutput.text = '';
+
+        var firstname = $('.content .content-inner form input[name=firstname]');
+        var lastname = $('.content .content-inner form input[name=lastname]');
+        var email = $('.content .content-inner form input[name=email]');
+        var username = $('.content .content-inner form input[name=username]');
+        var password = $('.content .content-inner form input[name=password]');
+
+        var fields = [firstname, lastname, email, username, password];
+        var allProvided = true;
+        fields.forEach(function(field) {
+            if(!field.val()) {
+                field.prev().text('This field is require!');
+                allProvided = false;
+            }else{
+                
+                if(field.attr('name') == 'password'){
+                    if(!(field.val().length >= 6)){
+                        field.prev().text('Password must be at least 6 characters!');
+                        allProvided = false;
+                    }
+                }else{
+                    field.prev().text('');
+                }
+            }
+        });
+
+        if(allProvided) {
+            window.location.href = './home.html';
+        }
     })
 })
