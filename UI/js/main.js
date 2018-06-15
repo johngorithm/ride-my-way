0d8ca5171ea3f5b9d9028b6e92037ddb2f7e3a28
John Obi <jovinxcreative@gmail.com>
2018-06-15 16:45:27 +0100

    optimized to display perfectly on the different screen sizes

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
 
+    //TOGGLING NAVBAR ON SMALL SCREENS
+    $('nav .navbar > p.navbar-toggle').on('click', function() {
+
+        $('nav.navigation .navbar .nav-right').addClass('animate').toggle();
+    })
+
+    $(window).on('resize', function() {
+        if($(this).width() > 700){
+            $('nav.navigation .navbar .nav-right').css('display', 'block');
+        }else{
+
+            if( $('nav.navigation .navbar .nav-right').css('display') == 'block') {
+                $('nav.navigation .navbar .nav-right').css('display', 'none');
+            }
+            
+        }
+    })
+
     //ADD OFFER FORM MODAL JS
     $('nav.navigation .navbar ul li.nav-item a[href="#"]').on('click', function(event) {
         event.preventDefault();
